e7281f521681ef8d44ad820e2d72ef403460ce5f
RosarioManny <rosario.emm47@gmail.com>
2024-09-16 15:25:48 -0400

    If statements for gameScore

--- js/map-1.js
+++ js/map-1.js
@@ -1,32 +1,46 @@
 const playerPicks = document.querySelectorAll(".choice")
 
 let inventory = [];
-let playerChoices = [];
+// let playerChoices = [];
 let gameScore = 0;
+
+// vvvvvv Restart Code vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv
 
 let init = () => {
     inventory = [];
-    playerChoices = [];
+    // playerChoices = []; // I think I don't need this variable because Inventory is basically the player's choices.
     gameScore = 0;   
 }
-
 init()
 
-
-// playerPicks.onclick = function(evt) {
-//     console.log(evt.target.innerText)
-// };
+// vvvvvv Player Choice Code vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv
 
 playerPicks.forEach((pick) => {
-    pick.addEventListener("click", (e) => {
+    pick.addEventListener("click", () => {
         console.log(pick.classList)
      if (pick.classList[0] == "choice") {
-        console.log("Hello")
-     } else {
-        console.log("Not Workin")
-     }
+        document.getElementById("mdlText").innerHTML += (`<li>${pick.textContent}</li>`) 
+        if (pick.classList[1] == "good") {
+            gameScore++
+            console.log(gameScore)
+        } else if (pick.classList[1] == "nuetral") {
+            gameScore += 2
+            console.log(gameScore)
+        } else if (pick.classList[1] == "bad") {
+            gameScore += 3
+            console.log(gameScore)
+        }
+        // *SCRAPPED* document.getElementById("mdlText").append(pick.textContent)
+     } 
     })
 });
+
+const nextScene = () => {
+
+}
+
+// vvvvvv Inventory Code vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv
+// The items that are put within the player's inventory
 
 // vvvvvv Game Ending Code vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv
 // Game ending will be decided by a value given to each choice. 
