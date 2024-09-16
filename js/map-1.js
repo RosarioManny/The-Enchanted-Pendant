975e7de0f133545e295e451a74745eedd225bf2f
RosarioManny <rosario.emm47@gmail.com>
2024-09-16 14:29:23 -0400

    Buttons

--- js/map-1.js
+++ js/map-1.js
@@ -1,13 +1,51 @@
-const buttonFirst = document.querySelector(".first")
+const playerPicks = document.querySelectorAll(".choice")
 
-let Inventory = [];
-let playerChoices = [
-    {item: "Banana"},
-    {item: "Apple"},
-    {item: "Melon"}
-];
+let inventory = [];
+let playerChoices = [];
+let gameScore = 0;
+
+let init = () => {
+    inventory = [];
+    playerChoices = [];
+    gameScore = 0;   
+}
+
+init()
 
 
-buttonFirst.addEventListener("click", (e) => {
-    console.log(e.target)
-})
+// playerPicks.onclick = function(evt) {
+//     console.log(evt.target.innerText)
+// };
+
+playerPicks.forEach((pick) => {
+    pick.addEventListener("click", (e) => {
+        console.log(pick.classList)
+     if (pick.classList[0] == "choice") {
+        console.log("Hello")
+     } else {
+        console.log("Not Workin")
+     }
+    })
+});
+
+// vvvvvv Game Ending Code vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv
+// Game ending will be decided by a value given to each choice. 
+// If value <= 4 = Good Ending; If value > 4 && < 7 = Nuetral Ending; If value > 8 = Bad Ending;
+
+// vvvvvv Inventory Modal Code vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv
+const modal = document.querySelector("#inventoryModal");
+const openInventory = document.querySelector("#inventory");
+const closeModal = document.querySelector(".close");
+
+openInventory.onclick = () => {
+    modal.style.display = "block"
+}
+
+closeModal.onclick = function() {
+    modal.style.display = "none";
+  }
+window.onclick = function(event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
+}
