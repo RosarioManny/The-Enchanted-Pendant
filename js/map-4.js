6d7a5d194b6b99192385563ff1aeff3cc0a45fb5
RosarioManny <rosario.emm47@gmail.com>
2024-09-19 13:47:11 -0400

    Web 4

--- js/map-4.js
+++ js/map-4.js
@@ -51,10 +51,10 @@ const handlePath = playerPicks.forEach((pick) => {
         if (pick.classList[0] == "choice") { 
 
             if (parseInt(localStorage.getItem("gameScore")) <= 5) {
-            setTimeout(window.location.href = "../map-final-G.html", 1)
+            setTimeout(window.location.href = "./map-final-G.html", 1)
 
         } else if (parseInt(localStorage.getItem("gameScore")) > 5) {
-            setTimeout(window.location.href = "../map-final-N.html", 1)
+            setTimeout(window.location.href = "./map-final-N.html", 1)
         }
         }
     })
